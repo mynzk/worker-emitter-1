934c04439a635f77ff93f3087209387b02830719
Saul-Mirone <xy610888871@gmail.com>
2019-12-16 00:02:20 +0800

    init

---
+++ README.md
@@ -0,0 +1,37 @@
+# Worker Emitter
+
+Play with web worker like event.
+
+## Usage
+
+Install
+
+```bash
+npm install worker-emitter
+```
+
+In worker:
+```javascript
+import { register } from "worker-emitter";
+
+const worker = register();
+
+worker.on('event-name', async (data) => {
+  const name = await handler(data);
+  return name; // <- name is 'WorkerEmitter'
+});
+```
+
+In main thread:
+
+```javascript
+import { WorkerEmitter } from "worker-emitter";
+
+const worker = new Worker('path/to/worker.js')
+
+const workerEmitter = new WorkerEmitter(worker);
+
+workerEmitter.emit('event-name', { foo: 'bar' }).then(res => {
+  console.log(res); // <- get 'WorkerEmitter'
+});
+```
---
+++ src/index.ts
@@ -0,0 +1,88 @@
+import uuid from "uuid/v4";
+
+function encode<T>(data: T): Uint16Array {
+  const str = JSON.stringify(data);
+  const buf = new ArrayBuffer(str.length * 2);
+  const bufView = new Uint16Array(buf);
+  bufView.set(str.split("").map((_, i) => str.charCodeAt(i)));
+  return bufView;
+}
+
+function decode<T = unknown>(buf: ArrayBufferLike): T {
+  return JSON.parse(
+    String.fromCharCode.apply(
+      null,
+      (new Uint16Array(buf) as unknown) as number[]
+    )
+  );
+}
+
+export type Data<T = unknown> = {
+  id: string;
+  type: string | number;
+  message: T;
+};
+
+export class WorkerEmitter {
+  private messageMap: Map<
+    string,
+    { callback: Function; type: string | number }
+  > = new Map();
+  constructor(private readonly worker: Worker) {
+    worker.onmessage = e => {
+      const { data } = e;
+      if (!data) return;
+
+      const { id, message } = decode(data);
+      const ret = this.messageMap.get(id);
+      if (!ret) return;
+
+      ret.callback(message);
+    };
+  }
+
+  emit<T, U>(type: string | number, message: T): Promise<U> {
+    return new Promise(resolve => {
+      const id = uuid();
+      const data = encode({
+        id,
+        type,
+        message
+      });
+      this.messageMap.set(id, {
+        type,
+        callback: (x: U) => {
+          resolve(x);
+        }
+      });
+      this.worker.postMessage(data.buffer, [data.buffer]);
+    });
+  }
+}
+
+type WorkerInstance = {
+  on(type: string, handler: Function): void;
+};
+
+export function register(): WorkerInstance {
+  const mapping: Record<string, Function> = {};
+  const post = (message: Data): void => {
+    const data = encode(message);
+    self.postMessage(data.buffer, [data.buffer]);
+  };
+  self.onmessage = async (e: MessageEvent) => {
+    const { data } = e;
+    if (!data) return;
+
+    const { type, id, message } = decode(data);
+
+    const result = (await mapping[type](message)) || "done";
+    post({ id, type, message: result });
+  };
+
+  return {
+    on: (type, handler) => {
+      mapping[type] = handler;
+    }
+  };
+}
